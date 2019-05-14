71bc26faedf919b69c523c9e882f14e799e6efd9
chhetrijanuka <janukachhetri15@gmail.com>
2019-05-14 10:57:37 +0530

    first commit

---
+++ love cal/loveCal.js
@@ -0,0 +1,4 @@
+  function checkloveper(){ 
+           	  var lovedata = Math.random() * 100;
+           	   lovedata = Math.floor(lovedata); 
+           	document.getElementById('lovevalue').value = lovedata + "%";} 
